76a1dd8211f2690b4a8dcf469ed3ed70a34421a5
Agustin Gigli <giglimagustin@gmail.com>
2023-02-11 21:33:28 -0300

    refactor the code

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,12 +1,18 @@
+import PropTypes from 'prop-types'
 import React from "react"
 
-const Result = ({value}) => {
+
+const Result = ({ value }) => {
   console.log("Renderización de Result", value)
   return (
     <div className="result">
       <span>{value}</span>
     </div>
   )
+}
+
+Result.propTypes = {
+  value: PropTypes.string.isRequired
 }
 
 export default Result
